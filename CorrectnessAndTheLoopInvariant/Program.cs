2a9d61f5cb65c5d3e0a96e1a1e891a1e6d7b6331
alitz <alitz1382@gmail.com>
2024-05-03 12:50:43 +0330

    add Algorithm insertionSort

---
+++ CorrectnessAndTheLoopInvariant/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+class Solution
+{
+    public static void insertionSort(int[] A)
+    {
+        var result = A.ToList();
+        for (int i = 0; i < result.Count; i++)
+        {
+            for (int j = i + 1; j < result.Count; j++)
+            {
+                if (result[i] < result[j]) continue;
+                else
+                {
+                    var temp = result[i];
+                    result[i] = result[j];
+                    result[j] = temp;
+                }
+            }
+        }
+        Console.WriteLine(string.Join(" ", result));
+    }
+
+    static void Main(string[] args)
+    {
+        int[] _ar = [7, 4, 3, 5, 6, 2];
+        insertionSort(_ar);
+    }
+}
+
